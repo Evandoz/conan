35a7f124731f0ad51df338f67d96ee3f1f651a5d
cstunao@gmail.com <Levan>
2019-06-13 17:55:25 +0800

    update data

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="menu">
-    <el-link href="/">主页</el-link>
+    <el-link href="">主页</el-link>
     <el-divider direction="vertical"></el-divider>
-    <el-link href="/plot">剧集</el-link>
+    <el-link href="plot">剧集</el-link>
     <el-divider direction="vertical"></el-divider>
-    <el-link href="/role">角色</el-link>
+    <el-link href="role">角色</el-link>
   </div>
 </template>
 
